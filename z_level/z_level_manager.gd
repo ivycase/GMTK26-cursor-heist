e02b4dce639e819f87813a452d8046526bc8885d
ivycase <case@berkeley.edu>
2026-07-26 01:26:51 -0700

    fix: enter vent while holding stuff

--- z_level/z_level_manager.gd
+++ z_level/z_level_manager.gd
@@ -12,20 +12,28 @@ enum Layers {BASE, VENT}
 @export var vent_level_parent: Node2D
 @export var vent_level_tilemap: TileMapLayer
 
+var yeet_zone: Vector2 = Vector2(5000, 5000)
+var yeet_parent: Node2D
+
 func _ready() -> void:
 	for vent in vents:
-		vent.entered_zone.connect(toggle_z_level.unbind(1))
+		vent.entered_zone.connect(toggle_z_level)
 		vent.top_level = true
 		
-	vent_level_parent.global_position += Vector2(5000, 5000) # just yeet is somewhere else lmfao
+	yeet_parent = Node2D.new()
+	yeet_parent.global_position = yeet_zone
+	vent_level_parent.reparent.call_deferred(yeet_parent) # just yeet is somewhere else lmfao
 
-func toggle_z_level() -> void:
+func toggle_z_level(_node: Node2D) -> void:
+	if Global.mouse_holder is DragComponent:
+		Global.mouse_holder.dragged_node.reparent.call_deferred(vent_level_parent if active_z_level == Layers.BASE else base_level_parent)
+	
 	match active_z_level:
 		Layers.BASE:
 			active_z_level = Layers.VENT
 			
-			base_level_parent.global_position += Vector2(5000, 5000) # just yeet is somewhere else lmfao
-			vent_level_parent.global_position -= Vector2(5000, 5000) # just unyeet is somewhere else lmfao
+			base_level_parent.reparent.call_deferred(yeet_parent) # just yeet is somewhere else lmfao
+			vent_level_parent.reparent.call_deferred(get_parent()) # just unyeet is somewhere else lmfao
 			
 			base_level_parent.hide()
 			base_level_tilemap.collision_enabled = false
@@ -36,8 +44,8 @@ func toggle_z_level() -> void:
 		Layers.VENT:
 			active_z_level = Layers.BASE
 			
-			vent_level_parent.global_position += Vector2(5000, 5000) # just yeet is somewhere else lmfao
-			base_level_parent.global_position -= Vector2(5000, 5000) # just unyeet is somewhere else lmfao
+			vent_level_parent.reparent.call_deferred(yeet_parent) # just yeet is somewhere else lmfao
+			base_level_parent.reparent.call_deferred(get_parent()) # just unyeet is somewhere else lmfao
 			
 			vent_level_parent.hide()
 			vent_level_tilemap.collision_enabled = false
