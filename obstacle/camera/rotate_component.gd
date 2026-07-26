044c066412dfb80803b763f9ba93de98f9ab7cef
ivycase <case@berkeley.edu>
2026-07-25 17:51:59 -0700

    fix: smoother camera

--- obstacle/camera/rotate_component.gd
+++ obstacle/camera/rotate_component.gd
@@ -1,43 +1,49 @@
 class_name RotateComponent
 extends Node2D
 
+@export var is_enabled: bool = true
 @export var rotate_node: Node2D
 @export var timer: Timer
 @export var delay: float = 2.5
 @export var look_targets: Array[Marker2D]
 @export var rotate_speed: float = 2.5
+@export_range(-5.0, 5.0) var ease_curve: float = 1.0
 
 var target_index: int = 0
 var target_angles: Array[float]
 var target: float
+var prev_target: float
 var rotate_interpolation: float = 0.0
 
 func _ready() -> void:
-	target_angles.append(global_rotation)
+	target_angles.append(rotate_node.global_rotation)
+	
 	for marker: Marker2D in look_targets:
-		target_angles.append(rotate_node.global_position.angle_to_point(marker.global_position))
+		target_angles.append(rotate_node.global_position.angle_to_point(marker.global_position) + rotate_node.global_rotation)
+	
+	target = rotate_node.global_rotation
+	_look_at_next_target()
 	
 	timer.wait_time = delay
 	timer.timeout.connect(_look_at_next_target)
-	
-	_look_at_next_target()
-	rotate_speed *= -1
-	rotate_node.global_rotation = target
-
-func _disable() -> void:
-	pass
 
 func _physics_process(delta: float) -> void:
+	if !is_enabled: return
+	
 	if rotate_interpolation >= 1:
 		if timer.is_stopped(): timer.start()
 		return
 		
-	rotate_node.global_rotation = rotate_toward(rotate_node.global_rotation, target, delta * rotate_speed)
+	rotate_node.global_rotation = lerp_angle(prev_target, target, ease(rotate_interpolation, ease_curve))
 	
-	rotate_interpolation += delta
+	rotate_interpolation += delta * rotate_speed
+	print(target_angles)
+	
+func disable() -> void:
+	is_enabled = false
 
 func _look_at_next_target() -> void:
 	target_index += 1
+	prev_target = target
 	target = target_angles[target_index % len(target_angles)]
 	rotate_interpolation = 0
-	rotate_speed *= -1
