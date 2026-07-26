7d8c6f09e3ebc96b5801d1f6254154788a438e99
ivycase <case@berkeley.edu>
2026-07-26 00:02:51 -0700

    feat: extra visuals

--- zone/button/button_component.gd
+++ zone/button/button_component.gd
@@ -1,5 +1,5 @@
 class_name ButtonComponent
-extends Node
+extends Node2D
 
 signal update_power(trigger_node: Node2D, new_power_setting: bool)
 
@@ -12,5 +12,17 @@ func _ready() -> void:
 	_late_ready()
 
 func _late_ready() -> void:
+	trigger_zone.entered_zone.connect(set.bind("is_powered_on", true).unbind(1))
+	trigger_zone.exited_zone.connect(set.bind("is_powered_on", false).unbind(1))
+	
 	trigger_zone.entered_zone.connect(update_power.emit.bind(true))
 	trigger_zone.exited_zone.connect(update_power.emit.bind(false))
+	
+	trigger_zone.entered_zone.connect(update_power_visual.unbind(1))
+	trigger_zone.exited_zone.connect(update_power_visual.unbind(1))
+	
+func update_power_visual() -> void:
+	if is_powered_on:
+		modulate = Color.DIM_GRAY
+	else:
+		modulate = Color.WHITE
